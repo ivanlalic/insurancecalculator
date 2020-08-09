2abb1b11f08fd5df910374ad79ea1a070176d53e
ivanlalic <ivanlalic@gmail.com>
2020-08-09 02:47:48 -0300

    Updates

---
+++ precache-manifest.d5a7a75bbe3d86331b9bdb39e9e7f5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdff9c9c96964a9a118640dac2323a5",
+    "url": "/ivanlalic/insurancecalculator/index.html"
+  },
+  {
+    "revision": "8b1e0872ae1143a90b05",
+    "url": "/ivanlalic/insurancecalculator/static/css/main.f7b4118e.chunk.css"
+  },
+  {
+    "revision": "5d1b921f09a46afe6628",
+    "url": "/ivanlalic/insurancecalculator/static/js/2.fc4aec72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ivanlalic/insurancecalculator/static/js/2.fc4aec72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1e0872ae1143a90b05",
+    "url": "/ivanlalic/insurancecalculator/static/js/main.eff3905f.chunk.js"
+  },
+  {
+    "revision": "563418e09e4424ca31bf",
+    "url": "/ivanlalic/insurancecalculator/static/js/runtime-main.2d95dceb.js"
+  }
+]);
